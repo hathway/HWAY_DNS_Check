6400071eff08bc258b72c990b90097d291c3e8ab
Vincent Khougaz <vincent@wearehathway.com>
2014-07-08 15:55:54 -0700

    Made parallel with gevent

--- dns_query.py
+++ dns_query.py
@@ -5,7 +5,12 @@ import dns
 import sys
 import dns.resolver
 import re
+import csv
 from os import path
+from gevent import monkey
+monkey.patch_all()
+from gevent.pool import Pool
+from random import shuffle
 
 ip_re = re.compile('.*?((?:[0-9]{1,3}\.){3}[0-9]{1,3})')
 
@@ -13,11 +18,16 @@ ip_re = re.compile('.*?((?:[0-9]{1,3}\.){3}[0-9]{1,3})')
 def resolve_ip(dns_server, domain):
     print("Resolving {} against DNS server {} ...".format(domain, dns_server))
     r = dns.resolver.Resolver()
+    r.lifetime = 5  # timeout
     r.name_servers = [dns_server]
     try:
         query = r.query(domain, 'a')
     except dns.resolver.NXDOMAIN:
         return 'NXDOMAIN'
+    except dns.exception.Timeout:
+        return 'TIMEOUT'
+    except dns.resolver.NoNameservers:
+        return 'NO NAMESERVERS'
 
     response = query.response.to_text().split('\n')
     # 'id 64407\nopcode QUERY\nrcode NOERROR\nflags QR RD RA\n;QUESTION\npython.org. IN A\n;ANSWER\npython.org. 1383 IN A 140.211.10.69\n;AUTHORITY\n;ADDITIONAL'
@@ -61,13 +71,27 @@ if __name__ == "__main__":
     with open(dns_list_file) as f:
         dns_list = [s.split(' ', 1) for s in f.read().split('\n') if s.count(' ') > 0]
 
-    # "DNS IP", "DNS DESC", "domain.com", "domain.com", ... \n
-    output = '"DNS IP", "DNS DESC", ' + ','.join('"{}"'.format(domain) for domain in domain_list) + '\n'
+    worker_pool = Pool(50)
+    work = []
+    for domain in domain_list:
+        for dns_ip, dns_desc in dns_list:
+            work.append((dns_ip, domain))
+    shuffle(work)
+    # Build up a list of (dns_ip, domain) to check, in random order
+
+    def wrap_work(inp):
+        # Takes a tuple of (dns_ip, domain) and returns (dns_ip, domain, resolved ip)
+        dns_ip, domain = inp
+        return dns_ip, domain, resolve_ip(dns_ip, domain)
+
+    results = {}
+    for dns_ip, domain, ip in worker_pool.imap_unordered(wrap_work, work):
+        results[(dns_ip, domain)] = ip
+        # Apply wrap_work to every item in work then store the results in a dict
 
     # "<dns ip>", "<dns desc>", "<ip>", "<ip>", ... \n
+    out = csv.writer(open(args.output, 'wb'))
+    out.writerow(["DNS IP", "DNS DESC"] + domain_list)
     for dns_ip, dns_desc in dns_list:
-        output += '"{}", "{}", {}'.format(dns_ip, dns_desc.strip(),
-                    ','.join('"{}"'.format(resolve_ip(dns_ip, domain)) for domain in domain_list)) + '\n'
-
-    with open(args.output, 'w') as f:
-        f.write(output)
+        # Look up and format every combination of dns and domain
+        out.writerow([dns_ip, dns_desc.strip()] + list(results[(dns_ip, domain)] if (dns_ip, domain) in results else "unknown" for domain in domain_list))
